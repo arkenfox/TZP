a3b0a5d993a68507cde36dc83cbf1e8cab4a9ad4
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-09-22 16:11:36 +0000

    promisify and section hash

--- js/misc.js
+++ js/misc.js
@@ -7,15 +7,23 @@ function reset_misc() {
 }
 
 function get_component_shims() {
+	let r
 	let shim = (typeof Components === "undefined") ? zU : Object.getOwnPropertyNames(Components.interfaces).join(", ")
 	dom.shimdata = shim
 	dom.shimdata.style.color = zshow
-	if (shim !== zU) {shim = sha1(shim) + s18 +"["+ shim.split(", ").length +"]"+ sc}
-	dom.shim.innerHTML = shim
+	if (shim == zU) {
+		r = zU
+	} else {
+		r = sha1(shim)
+		shim = r + s18 +"["+ shim.split(", ").length +"]"+ sc
+	}
+	dom.shim.innerHTML = r
+	return "shims: " + r
 }
 
 function get_iframe_props() {
 	/* https://github.com/abrahamjuliot/creepjs */
+	let r
 	try {
 		// create iframe & append
 		let id = "iframe-window-version"
@@ -32,19 +40,26 @@ function get_iframe_props() {
 		// output
 		dom.iProps.innerHTML = sha1(props.join()) + s18 +"["+ props.length +"]"+sc
 		//console.debug(props.join("\n"))
+		r= sha1(props.join())
 	} catch(e) {
 		dom.iProps.innerHTML = error_iframe
+		r = "error"
 	}
+	return "iframe: " + r
 }
 
 function get_int_observer() {
 	let callback = function(entries, observer) {}
+	let r
 	try {
 		let observer = new IntersectionObserver(callback)
 		dom.intObserver = zE
+		r= zE
 	} catch(e) {
 		dom.intObserver = zD
+		r = zD
 	}
+	return "int observer: " + r
 }
 
 function get_mathml(type) {
@@ -63,18 +78,23 @@ function get_mathml(type) {
 	let pre = " | offsetHeight difference: ",
 		post = (diff < 10 ? tb_safer : tb_standard)
 	dom.mathml.innerHTML = (diff < 10 ?	zD : zE) + pre + diff + post
+	return "mathml: " + diff
 }
 
 function get_nav_prototype() {
+	let r
 	try {
 		let nProto = Object.keys(Object.getPrototypeOf(navigator)).join(", ")
 		dom.nProto.innerHTML = sha1(nProto) + s18 +"["+ nProto.split(', ').length +"]"+ sc
 		dom.nProto2 = nProto
 		dom.nProto2.style.color = zshow
+		r = sha1(nProto)
 	} catch(e) {
-		dom.nProto.innerHTML = zB
+		r = zB0
+		dom.nProto = r
 		dom.nProto2 = ""
 	}
+	return "navigator: " + r
 }
 
 function get_recursion() {
@@ -100,19 +120,45 @@ function get_recursion() {
 
 function get_reporting_api() {
 	// FF65+
+	let r
 	try {
 		let observer = new ReportingObserver(function() {})
 		dom.reportingAPI = zE
+		r = zE
 	} catch(e) {
 		if (isFF) {
-			dom.reportingAPI = (isVer > 64 ? zD : zNS )
+			r = (isVer > 64 ? zD : zNS )
+			dom.reportingAPI = r
 		} else {
+			r = zD + " or " +zNS
 			dom.reportingAPI = zD+" [or "+zNS+"]"
 		}
 	}
+	return "reporting: " + r
 }
 
-function get_perf() {
+function get_perf1() {
+	// mark (seems only RFP affects this)
+	let r1 = ""
+	try {
+		performance.mark("test")
+		if (performance.mark === undefined) {
+			r1 = zNA
+		} else {
+				performance.mark("a")
+				r1 = performance.getEntriesByName("a","mark").length
+					+ ", " + performance.getEntries().length
+					+ ", " + performance.getEntries({name:"a", entryType:"mark"}).length
+					+ ", " + performance.getEntriesByName("a","mark").length
+				performance.clearMarks()
+		}
+	} catch(e) {r1 = zB0}
+	dom.perf1.innerHTML = r1 + (r1 == "0, 0, 0, 0" ? rfp_green: rfp_red)
+	r1 = (r1 == "0, 0, 0, 0" ? "zero" : "not zero")	
+	return "perf.mark: " + r1
+}
+
+function get_perf2() {
 	let i = 0,
 		result = true,
 		times = [],
@@ -131,43 +177,31 @@ function get_perf() {
 	let check = setInterval(run, 13)
 }
 
-function get_perf2() {
+function get_perf3() {
 	// loadEventEnd (also dom.enable_performance)
-	let r3 = ""
+	let r = ""
 	try {
 		let timing = performance.timing
-		r3 = timing.navigationStart - timing.loadEventEnd
-		r3 = (r3 == 0 ? "zero" : "not zero")
-	} catch(e) {r3 = zB}
-	dom.perf3.innerHTML = r3
+		r = timing.navigationStart - timing.loadEventEnd
+		r = (r == 0 ? "zero" : "not zero")
+	} catch(e) {r = zB0}
+	dom.perf3.innerHTML = r
+	return "perf.timing: " + r
+}
 
+function get_perf4() {
 	// also: dom.enable_performance_navigation_timing
-	let r4 = zD
+	let r = zD
 	try {
-		if (window.PerformanceNavigationTiming) {r4 = zE}
-		if (isVer > 77) {
-			r4 += (r4 == zD ? rfp_green : rfp_red) //78+: 1511941
-		}
-	} catch(e) {r4 = zB}
-	dom.perf4.innerHTML = r4
+		if (window.PerformanceNavigationTiming) {r = zE}
+	} catch(e) {r = zB0}
 
-	// mark (seems only RFP affects this)
-	let r1 = ""
-	try {
-		performance.mark("test")
-		if (performance.mark === undefined) {
-			r1 = "not supported"
-		} else {
-				performance.mark("a")
-				r1 = performance.getEntriesByName("a","mark").length
-					+ ", " + performance.getEntries().length
-					+ ", " + performance.getEntries({name:"a", entryType:"mark"}).length
-					+ ", " + performance.getEntriesByName("a","mark").length
-				performance.clearMarks()
-				r1 += (r1 == "0, 0, 0, 0" ? rfp_green: rfp_red)
-		}
-	} catch(e) {r1 = zB}
-	dom.perf1.innerHTML = r1
+	if (isVer > 77) {
+		dom.perf4.innerHTML = r + (r == zD ? rfp_green : rfp_red) //78+: 1511941
+	} else {
+		dom.perf4 = r
+	}
+	return "perf navigation: " + r
 }
 
 function get_svg() {
@@ -184,9 +218,11 @@ function get_svg() {
 	s.appendChild(c)
 	dom.svgDiv.appendChild(s)
 	// output
-	dom.svgBasicTest = (dom.svgDiv.offsetHeight > 0 ? zE : zD)
+	let r = (dom.svgDiv.offsetHeight > 0 ? zE : zD)
+	dom.svgBasicTest = r
 	// remove
 	dom.svgDiv.removeChild(s)
+	return "svg: " + r
 }
 
 function get_wasm() {
@@ -201,43 +237,82 @@ function get_wasm() {
 		return false
 	})()
 	// ToDo: dom.wasm.innerHTML = (supported ? zE+tb_standard : zD+tb_safer) // currently alpha
-	dom.wasm = (supported ? zE : zD )
+	let r = (supported ? zE : zD )
+	dom.wasm = r
+	return "wasm: " + r
 }
 
 function get_windowcontent() {
+	let r
 	try {
 		let test = window.content
 		let test2 = content.name
-		return zE
+		r = zE
 	} catch(e) {
-		return zD
+		r = zD
 	}
+	dom.wincon = r
+	return "window.content: " + r
 }
 
 function outputMisc(type) {
 	let t0 = performance.now()
-	// one-liners
-	try {dom.nBeacon = (navigator.sendBeacon ? zE : zD)} catch(e) {dom.nBeacon.innerHTML = zB}
-	try {dom.webshare = (navigator.share ? zE : zD)} catch(e) {dom.webshare.innerHTML = zB}
-	dom.nClipboard = ("clipboard" in navigator ? zE: zD) // FF63+
-	dom.reqIdleCB = ("requestIdleCallback" in window ? zE: zD)
-	dom.mediaSession = ("mediaSession" in navigator ? zE: zD) // FF71+
-	dom.webauth = ("credentials" in navigator ? zE: zD) +" | "+ ("u2f" in window ? zE: zD)
-	dom.wincon = get_windowcontent()
+	let misc = [], r = ""
+
+	try {r = (navigator.sendBeacon ? zE : zD)} catch(e) {r = zB0}
+	dom.nBeacon = r
+	misc.push("beacon: " + r)
+
+	try {r = (navigator.share ? zE : zD)} catch(e) {r = zB0}
+	dom.webshare = r
+	misc.push("web share: " + r)
+	
+	r = ("clipboard" in navigator ? zE: zD) // FF63+
+	dom.nClipboard = r
+	misc.push("clipboard: " + r)
+
+	r = ("requestIdleCallback" in window ? zE: zD)
+	dom.reqIdleCB = r
+	misc.push("requestIdleCallback: " + r)
+
+	r = ("mediaSession" in navigator ? zE: zD) // FF71+
+	dom.mediaSession = r
+	misc.push("mediaSession: " + r)
+
+	r = ("credentials" in navigator ? zE: zD) +" | "+ ("u2f" in window ? zE: zD)
+	dom.webauth = r
+	misc.push("webauth + u2f: " + r)
+
 	// other
-	get_component_shims()
-	get_iframe_props()
-	get_int_observer()
-	get_mathml(type)
-	get_nav_prototype()
-	get_reporting_api()
-	get_svg()
-	get_wasm()
-	get_perf()
+	Promise.all([
+		get_perf1(),
+		get_perf3(),
+		get_perf4(),
+		get_component_shims(),
+		get_iframe_props(),
+		get_int_observer(),
+		get_windowcontent(),
+		get_nav_prototype(),
+		get_reporting_api(),
+		get_wasm(),
+		get_mathml(type),
+		get_svg()
+	]).then(function(result){
+		for (let i=0; i < 11; i++) {
+			misc.push(result[i])
+		}
+		misc.sort()
+		console.log("misc", misc)
+		dom.mischash = sha1(misc.join())
+		// perf
+		debug_page("perf","misc",t0,gt0)
+	})
+
+	// perf2 is not needed in the hash as performance.mark is RFP unique
 	get_perf2()
+	// ToDO: experimental: bucketize?
 	get_recursion()
-	// perf
-	debug_page("perf","misc",t0,gt0)
+
 }
 
 outputMisc("load")
