0c5b11b429c3a23ae281735b5fdb93e7686167d8
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-09-30 14:08:52 +0000

    add delimiter

--- js/workershared_ua.js
+++ js/workershared_ua.js
@@ -17,7 +17,7 @@ onconnect = function(e) {
 			try {r = navigator[list[i]]} catch(e) {r = (e.name == "ReferenceError" ? zB1 : zB2)}
 			if (r == "") {r = "undefined"}
 			if (r == undefined && e.data) {r = zB3}
-			res.push((i).toString().padStart(2,"0")+" "+r)
+			res.push((i).toString().padStart(2,"0")+": "+r)
 		}
 		port.postMessage(res)
 	}
