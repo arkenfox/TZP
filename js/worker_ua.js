4e16ec8bc208b34cc02fb7b3870e0e2209b038ec
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-10-01 09:24:51 +0000

    match screen.js

--- js/worker_ua.js
+++ js/worker_ua.js
@@ -10,6 +10,7 @@ addEventListener("message", function(e) {
 	// e.data = isFF
 	for(let i=0; i < list.length; i++) {
 		try {r = navigator[list[i]]} catch(e) {r = (e.name == "ReferenceError" ? zB1 : zB2)}
+		if (r == zU) {r = "\"undefined\""}
 		if (r == "") {r = "undefined"}
 		if (r == undefined && e.data) {r = zB3}
 		res.push((i).toString().padStart(2,"0")+": "+r)
