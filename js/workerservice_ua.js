cf5a74259a9f77ba674ba7069cd47eac7a0ab476
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-10-01 13:27:54 +0000

    match screen.js

--- js/workerservice_ua.js
+++ js/workerservice_ua.js
@@ -5,14 +5,13 @@ addEventListener("message", function(e) {
 		res = [],
 		zB1 = "script blocked [a]",
 		zB2 = "script blocked [b]",
-		zB3 = "script blocked [c]",
 		r = ""
 	// e.data = isFF
 	for(let i=0; i < list.length; i++) {
 		try {r = navigator[list[i]]} catch(e) {r = (e.name == "ReferenceError" ? zB1 : zB2)}
-		if (r == zU) {r = "\"undefined\""}
-		if (r == "") {r = "undefined"}
-		if (r == undefined && e.data) {r = zB3}
+		if (r == "") {r = "empty string"}
+		if (r == "undefined") {r = "undefined string"}
+		if (r == undefined) {r = "undefined value"}
 		res.push((i).toString().padStart(2,"0")+": "+r)
 	}
 	let channel = new BroadcastChannel("sw-ua");
