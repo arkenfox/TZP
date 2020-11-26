0aef2a8e1d7880ff835878f6ed0e034c55243ced
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-11-26 23:02:34 +0000

    match new screen.js

--- js/workerservice_ua.js
+++ js/workerservice_ua.js
@@ -3,17 +3,17 @@
 addEventListener("message", function(e) {
 	let list = ['userAgent','appCodeName','appName','product','appVersion','platform'],
 		res = [],
-		zB1 = "script blocked [a]",
-		zB2 = "script blocked [b]",
 		r = ""
-	// e.data = isFF
+	let isFF = e.data
 	for(let i=0; i < list.length; i++) {
-		try {r = navigator[list[i]]} catch(e) {r = (e.name == "ReferenceError" ? zB1 : zB2)}
+		try {r = navigator[list[i]]} catch(e) {r = "blocked"}
 		if (r == "") {r = "empty string"}
 		if (r == "undefined") {r = "undefined string"}
+		if (r == undefined && isFF) {r = "blocked"}
 		if (r == undefined) {r = "undefined value"}
-		res.push((i).toString().padStart(2,"0")+": "+r)
+		res.push(list[i]+":"+r) // no spaces
 	}
+	res.sort() // always sort
 	let channel = new BroadcastChannel("sw-ua");
 	channel.postMessage({msg: res});
 }, false)
