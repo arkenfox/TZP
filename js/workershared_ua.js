221a2b93c4748fab023a5b62fa4dead9600deef5
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-11-26 23:02:58 +0000

    match new screen.js

--- js/workershared_ua.js
+++ js/workershared_ua.js
@@ -8,17 +8,17 @@ onconnect = function(e) {
 	port.onmessage = function(e) {
 		let list = ['userAgent','appCodeName','appName','product','appVersion','platform'],
 			res = [],
-			zB1 = "script blocked [a]",
-			zB2 = "script blocked [b]",
-			r =""
-		// e.data = isFF
+			r = ""
+		let isFF = e.data
 		for(let i=0; i < list.length; i++) {
-			try {r = navigator[list[i]]} catch(e) {r = (e.name == "ReferenceError" ? zB1 : zB2)}
+			try {r = navigator[list[i]]} catch(e) {r = "blocked"}
 			if (r == "") {r = "empty string"}
 			if (r == "undefined") {r = "undefined string"}
+			if (r == undefined && isFF) {r = "blocked"}
 			if (r == undefined) {r = "undefined value"}
-			res.push((i).toString().padStart(2,"0")+": "+r)
+			res.push(list[i]+":"+r) // no spaces
 		}
+		res.sort() // always sort
 		port.postMessage(res)
 	}
 }
