1c9433e0199e0febef5b130f7b6e7483de9ee03d
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2021-01-31 03:15:31 +0000

    catch more blockage

--- js/language.js
+++ js/language.js
@@ -521,8 +521,12 @@ function get_lang_doc() {
 		for (let i=0; i < 49; i++) {
 			let result = get_item(i)
 			if (isFF) {
-				if (result == undefined) {result = zB0; err.push(i +" [unexpected]: undefined")}
-				if (result == "undefined") {result = zB0; err.push(i +" [unexpected]: \"undefined\"")}
+				if (result == undefined) {result = zB0; err.push(i +" [unexpected]: undefined")
+				} else if (result == "undefined") {result = zB0; err.push(i +" [unexpected]: \"undefined\"")
+				} else if (result == "") {result = zB0; err.push(i +" [unexpected]: zero-length string")
+				} else if (result == " | ") {result = zB0 + " | " + zB0; err.push(i +" [unexpected]: zero-length string")
+				} else if (result == " |  | ") {result = zB0 + " | " + zB0 + " | " + zB0; err.push(i +" [unexpected]: zero-length string")
+				}
 			}
 			res.push(result)
 			// output
