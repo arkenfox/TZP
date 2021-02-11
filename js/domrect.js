6d42856e00e762632655ff376079ef4e5acb12b3
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2021-02-11 04:19:46 +0000

    refactor: add known value lie, output known lies

--- js/domrect.js
+++ js/domrect.js
@@ -1,8 +1,9 @@
 "use strict";
 
 /* code based on work by kkapsner and canvasblocker
-	 https://canvasblocker.kkapsner.de/test/
-	 https://github.com/kkapsner/CanvasBlocker */
+https://canvasblocker.kkapsner.de/test/
+https://github.com/kkapsner/CanvasBlocker
+*/
 
 function reset_domrect() {
 	for (let i=0; i < 4; i++) {
@@ -20,32 +21,50 @@ function outputDomRect() {
 		// debug
 		//run0.sort(); run1.sort(); console.log("domrect\n - " + run0.join("\n - ") + "\n - " + run1.join("\n - "))
 
+		let knownHash = "bebfc291e9d1adc90e240790551d0305b8b91294"
+		let pretty = ["Element.getClientRects","Element.getBoundingClientRect","Range.getClientRects","Range.getBoundingClientRect"]
 		let hash = []
+
 		for (let i=0; i < 4; i++) {
+
+			// repurpose known arrays as lie booleans
+			if (isFF) {
+				// FF: rect6
+				// note: errors/undefined override these when we check value1
+				known["dr"+i] = (sha1( known["dr"+i].join()) == knownHash ? false : true)
+				//if (known["dr"+i] == true) {console.debug("lie: known values: "+ pretty[i])}
+			} else {
+				// non-FF
+				known["dr"+i] = false
+			}
+
 			let value1 = run0[i].split(":")[2],
 				value2 = run1[i].split(":")[2]
 			let push = value1
 			let display = value1
 
 			// simulate random
-			//if (i == 0 || i == 2) {value1 = "0c4cf7ff544ab66"; value2 = "ec46b57d8a484a"}
+			//if (i == 1) {value1 = "0c4cf7ff544ab66"; value2 = "ec46b57d8a484a"}
+			//if (i == 3) {value1 = "0c4cf7ff544ab66"; value2 = "ec46b57d8a484a"}
 
 			if (value1 == "tzp") {
-				// catch all error starts with "tzp:"
+				// errors: starts with "tzp:": don't record lie
+				known["dr"+i] = false
 				value1 = run0[i].split(":")[3]
 				if (value1 == "") {value1 = "error"}
 				if (value1 == undefined) {value1 = "undefined"}
 				push = value1
 				display = value1
 			} else if (value1 !== value2) {
-				// random on each pass
+				// random on each pass: record lie
+				known["dr"+i] = true
+				//if (known["dr"+i] == true) {console.debug("lie: random each pass: "+ pretty[i])}
 				push = "random"
 				display = sColor +"[1] "+sc + value1.substring(0,13) + ".. "
 					+ sColor +"[2] "+sc + value2.substring(0,14) + ".."
 					+ sColor + note_random
 			} else {
-				// same value
-				let isLies = false
+				// same value on two passes
 				// check for noise
 				if (isFF || isEngine == "blink") {
 					let compare = chk["dr"+i]
@@ -57,19 +76,29 @@ function outputDomRect() {
 							let delim = item.split(":")
 							if (prev_item == delim[0] +"_"+ delim[1]) {
 								let diff = (delim[3]-prev_value)
-								if (diff !== 0.25) {isLies = true}
+								if (diff !== 0.25) {
+									// record lie
+									known["dr"+i] = true
+									//if (known["dr"+i] == true) {console.debug("lie: shift: "+ pretty[i])}
+								}
 								let margin = (0.25 - diff)
 								diffs.push(prev_item +", "+ diff +", "+ margin +", "+ prev_value + ", "+ delim[3])
 							}
 							prev_item = delim[0] +"_"+ delim[1]
 							prev_value = delim[3]
 						})
-						if (isLies) {
-							console.log("DOMRect method dr" + i + " [item, diff, diff from 0.25, 1st measurement, shifted measurement]\n", diffs)
+						if (known["dr"+i] == true) {
+							//console.log("DOMRect method dr" + i + " [item, diff, diff from 0.25, 1st measurement, shifted measurement]\n", diffs)
 							push = "tampered"
 							display = value1 + sColor + note_noise
 						}
 					}
+				}
+			}
+			// global lies
+			if (!sRerun) {
+				if (known["dr"+i] == true) {
+					knownLies.push("domrect:" + pretty[i])
 				}
 			}
 			// push & display
@@ -82,32 +111,40 @@ function outputDomRect() {
 		section_info("domrect", t0, hash)
 	}
 
-	function getElements(){
-		return Array.from(document.querySelectorAll(".testRect"))
+	function getElements(classname){
+		return Array.from(document.querySelectorAll(classname))
 	}
 	function createTest(method, runtype, runarray, callback){
 		const properties = ["x","y","width","height","top","left","right","bottom"]
 		function performTest(runtype){
 			try {
-				const rects = getElements().map(callback)
+				let classname = ".testRect"
+				if (runtype == 3) {classname = ".knownRect"}
+				const rects = getElements(classname).map(callback)
 				const data = new Float64Array(rects.length * properties.length)
 				rects.forEach(function(rect, i){
 					properties.forEach(function(property, j){
 						data[i * properties.length + j] = rect[property]
-						// diffs on runs
-						// runtype !== 1: only collect 2 runs: 0 (first) + 2 (shifted by css)
-						if (runtype !== 1) {
-							// what is i (0 to 5) ? the element rect number: rect3 = option/select
-							let go = false
-							// everything except width/height
-							if (j !== 2 && j !== 3) {go = true}
-							// blink false positives: rect3 <option> left/right/x
-							if (isEngine == "blink" && i == 3) {
-								if (j == 5 || j == 6 || j == 0) {go = false}
-							}
-							if (go) {
-								let str = properties[j] +":rect"+ i +":run"+ runtype +":"+ rect[property]
-								chk[method].push(str)
+						if (runtype == 3) {
+							// known
+							let str = properties[j] +":"+ rect[property]
+							known[method].push(str)
+						} else {
+							// diffs on runs
+							// runtype !== 1: only collect 2 runs: 0 (first) + 2 (shifted by css)
+							if (runtype !== 1) {
+								// what is i (0 to 5) ? the element rect number: rect3 = option/select
+								let go = false
+								// everything except width/height
+								if (j !== 2 && j !== 3) {go = true}
+								// blink false positives: rect3 <option> left/right/x
+								if (isEngine == "blink" && i == 3) {
+									if (j == 5 || j == 6 || j == 0) {go = false}
+								}
+								if (go) {
+									let str = properties[j] +":rect"+ i +":run"+ runtype +":"+ rect[property]
+									chk[method].push(str)
+								}
 							}
 						}
 					})
@@ -118,7 +155,9 @@ function outputDomRect() {
 					properties.map(function(property){
 						return rects.map(function(rect, i){
 							item++
-							document.getElementById(method+item).textContent = rect[property]
+							try { // don't display rect6
+								document.getElementById(method+item).textContent = rect[property]
+							} catch(e) {}
 							return rect[property]
 						}).join("")
 					}).join("")
@@ -165,13 +204,15 @@ function outputDomRect() {
 	}
 
 	let t0 = performance.now()
-	let run0 = [], run1 = [], run2 = []
-	let chk = {dr0: [ ], dr1: [ ], dr2: [ ], dr3: [ ]}
+	let run0 = [], run1 = [], run2 = [], run3 = []
+	let chk = {dr0: [], dr1: [], dr2: [], dr3: []}
+	let known = {dr0: [], dr1: [], dr2: [], dr3: []}
 
 	Promise.all([
 		run(0, run0),
 		run(1, run1),
-		run(2, run2)
+		run(2, run2),
+		run(3, run3)
 	]).then(function(){
 		analyze()
 	})
