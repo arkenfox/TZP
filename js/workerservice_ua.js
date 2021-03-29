532fe905d4a1c60d111ae6fbf5cce399d55ca72c
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2021-03-29 05:03:25 +0000

    Update workerservice_ua.js

--- js/workerservice_ua.js
+++ js/workerservice_ua.js
@@ -4,14 +4,13 @@ addEventListener("message", function(e) {
 	let list = ['userAgent','appCodeName','appName','product','appVersion','platform'],
 		res = [],
 		r = ""
-	let isFF = e.data
 	for(let i=0; i < list.length; i++) {
 		try {r = navigator[list[i]]} catch(e) {r = "blocked"}
-		if (r == "") {r = "empty string"}
-		if (r == "undefined") {r = "undefined string"}
-		if (r == undefined && isFF) {r = "blocked"}
-		if (r == undefined) {r = "undefined value"}
-		res.push(list[i]+":"+r) // no spaces
+		if (r == "") {r = "empty string"
+		} else if (r == "undefined") {r = "undefined string"
+		} else if (r == undefined) {r = "undefined value"
+		}
+		res.push(list[i] +":"+ r) // no spaces
 	}
 	res.sort() // always sort
 	let channel = new BroadcastChannel("sw-ua");
