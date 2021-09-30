4b2c386a9454308a91855e983eda18dbbbf9097c
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2021-09-30 16:26:56 +0000

    tidy

--- js/elements.js
+++ js/elements.js
@@ -3,31 +3,30 @@
 function get_element_keys() {
 	let sName = "elements_element_keys"
 	clearDetail(sName)
-
-  return new Promise(resolve => {
-    try {
-      // create element
-      const id = 'html-element-version'
-      const element = document.createElement('div')
-      element.setAttribute('id', id)
-      // append element to dom
-      document.body.appendChild(element)
-      // get rendered element
-      const htmlElement = document.getElementById(id)
-      // get property keys in element object
-      const keys = []
-      for (const key in htmlElement) {
-        keys.push(key)
-      }
+	return new Promise(resolve => {
+		try {
+			// create element
+			const id = 'html-element-version'
+			const element = document.createElement('div')
+			element.setAttribute('id', id)
+			// append element to dom
+			document.body.appendChild(element)
+			// get rendered element
+			const htmlElement = document.getElementById(id)
+			// get property keys in element object
+			const keys = []
+			for (const key in htmlElement) {
+				keys.push(key)
+			}
 			sDetail[sName] = keys
 			let hash = sha1(keys.join())
 			dom.elementkeys.innerHTML = hash + buildButton("15", sName, keys.length)
-      return resolve("element_keys:"+ hash)
-    } catch (error) {
+			return resolve("element_keys:"+ hash)
+		} catch (error) {
 			dom.elementkeys.innerHTML = zB0
-      return resolve("element_keys:"+ zB0)
-    }
-  })
+			return resolve("element_keys:"+ zB0)
+		}
+	})
 }
 
 function outputElements() {
