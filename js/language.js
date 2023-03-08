34353a5669e9ceb8a2f27799ba50090721abe13d
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2023-03-08 03:42:57 +0000

    tidy

--- js/language.js
+++ js/language.js
@@ -156,11 +156,6 @@ function get_navigator() {
 
 		// DNT
 		let dnt = "", dntLie = false
-		try {
-			let dntTest = navigator.doNotTrack
-			if (gRun) {log_debug("dnt", cleanFn(dntTest))}
-		} catch(e) {}
-		
 		if (check_navKey("doNotTrack")) {
 			try {
 				dnt = navigator.doNotTrack
