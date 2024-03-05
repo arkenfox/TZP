efa72d765407e09f54abb2f584e0eccd60069e68
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2024-03-05 05:46:07 +0000

    tweak

--- js/worker_ua.js
+++ js/worker_ua.js
@@ -17,7 +17,7 @@ addEventListener("message", function(e) {
 		} else if (item === undefined || item === true || item === false || item === null) {item += ""
 		} else if (item == "") {item = "empty string"
 		} else if (typeof item == "string") {
-			if (!isNaN(item*1)) {item = "\"" + item + "\""}
+			if (!Number.isNaN(item*1)) {item = "\"" + item + "\""}
 		}
 		return item
 	}
