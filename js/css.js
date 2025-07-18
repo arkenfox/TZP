4c35838e57d857756d14f2b22904b646be472982
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2025-07-18 23:54:02 +0000

    fixup

--- js/css.js
+++ js/css.js
@@ -391,11 +391,12 @@ function get_media_css(METRIC) {
 		let oTests = {
 		// expected
 			'hover': {id: 'H', test: ['hover','none']},
-			'any-hover': {id: 'H', test: ['hover','none']},
+			'any-hover': {id: 'AH', test: ['hover','none']},
 			'prefers-reduced-motion': {id: 'PRM', test: [np,'reduce'], rfp: np, rfpver: 1}, // FF63+: 1478158
 			'pointer': {id: 'P', test: ['fine','coarse', 'none']}, // FF64+
-				// ^ this is FINE over COARSE | do not change the order
-			'any-pointer': {id: 'AP', test: ['coarse','fine','none'], rfp: 'fine + fine', rfpver: 1}, // FF64+
+			'any-pointer': {id: 'AP', test: ['fine','coarse','none'], rfp: 'fine + fine', rfpver: 1}, // FF64+
+				// ^ any-pointer: DO NOT CHANGE ORDER
+				// this is the 2nd value (we check :after: by default)
 			'prefers-contrast': {id: 'PC', test: [np,'less','more','custom'], rfp: np, rfpver: 1}, // FF101+: 1656363
 			'prefers-color-scheme': {id: 'PCS', test: ['light','dark'], rfp: 'light', rfpver: 1}, // FF67+: 1494034 | and see 1643656
 			'forced-colors': {id: 'FC', test: ['none','active']}, // FF89+: 1659511
@@ -453,10 +454,12 @@ function get_media_css(METRIC) {
 					// 'any-pointer, more than one of the values can match' / none = only if the others are not present
 					// COARSE over FINE: the first check was FINE over COARSE
 					let value2 = zNA, miniTest = ['coarse','fine','none']
+						// ^ any-pointer: DO NOT CHANGE ORDER
+						// this is the 1st value (to match :before:)
 					for (let i=0; i < miniTest.length; i++) {
 						if (window.matchMedia('('+ metric +':'+ aTest[i] +')').matches) {value2 = aTest[i]; break}
 					}
-					value = value += ' + '+ value2
+					value = value2 += ' + '+ value
 				}
 			} catch(e) {
 				if(!isTest) {log_error(14, METRIC +'_'+ metric, e)}
@@ -469,6 +472,7 @@ function get_media_css(METRIC) {
 				let cssvalue = getElementProp(14, '#css'+ oTests[metric].id, metric +'_css')
 				// don't concat errors
 				if ('any-pointer' == metric && cssvalue !== zErr) {
+					// this is the 1st value - we use :before
 					let cssvalue2 = getElementProp(7, '#css'+ oTests[metric].id, metric +'_css', ':before')
 					cssvalue = cssvalue2 == zErr ? zErr : cssvalue2 + cssvalue
 				}
