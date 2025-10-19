d2ddf7dd39a5fd9c36455d1a904c2b23463ebd87
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2025-10-19 16:03:26 +0000

    track success of stylesheet rules via JS

--- js/globals.js
+++ js/globals.js
@@ -162,6 +162,7 @@ let isArch = true,
 		'cursive','emoji','fangsong','fantasy','math','monospace',
 		'sans-serif','serif','system-ui','ui-monospace','ui-rounded','ui-serif'
 	],
+	isStylesheet, // stylesheet screen/window rules added: string showing range
 	isSystemFont = [],
 	isVer = 0,
 	isVerExtra = '',
