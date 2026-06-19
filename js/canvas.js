52ffb9b8bdf35358d4675f3e08eb21ce9c83af25
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2026-06-19 05:13:12 +0000

    tweak

--- js/canvas.js
+++ js/canvas.js
@@ -16,29 +16,168 @@ function check_canvas_to(data) {
 	return false
 }
 
+function get_canvas_info(target) {
+	// ToDo:
+		// pass the base64 from to* tests
+		// ^ no point getting it twice, plus we need to control the method used to get it
+		// this allows us to detect the number of chunks and definitively gives away FF protection
+		// we can also return the colorType: e.g. 6 = sRGB
+
+	if (undefined == target) {target = dom.tzpCanvasTo}
+	// https://stackoverflow.com/a/37997175
+	var PNG = {
+		parse: function(imgTag) {
+			var base64 = PNG.asBase64(imgTag);
+			var byteData = PNG.utils.base64StringToByteArray(base64);
+			var parsedPngData = PNG.utils.parseBytes(byteData);
+			return PNG.utils.enrichParsedData(parsedPngData);
+		},
+		asBase64: function(imgTag) {
+			var canvas = document.createElement("canvas");
+			canvas.width = imgTag.width; 
+			canvas.height = imgTag.height; 
+			var ctx = canvas.getContext("2d"); 
+			ctx.drawImage(imgTag, 0, 0); 
+			var dataURL = canvas.toDataURL("image/png"); // we already have this info
+			return dataURL.split('base64,')[1];
+		},
+		utils: {
+			base64StringToByteArray: function(base64String) {
+				//http://stackoverflow.com/questions/16245767/creating-a-blob-from-a-base64-string-in-javascript
+				var byteCharacters = atob(base64String);
+				var byteNumbers = new Array(byteCharacters.length);
+				for (var i = 0; i < byteCharacters.length; i++) {
+					byteNumbers[i] = byteCharacters.charCodeAt(i);
+				}
+				return new Uint8Array(byteNumbers);
+			},
+			parseBytes: function(bytes) {
+				var pngData = {};
+				//see https://en.wikipedia.org/wiki/Portable_Network_Graphics
+				//verify file header
+				pngData['headerIsValid'] = bytes[0] == 0x89
+					&& bytes[1] == 0x50
+					&& bytes[2] == 0x4E
+					&& bytes[3] == 0x47
+					&& bytes[4] == 0x0D
+					&& bytes[5] == 0x0A
+					&& bytes[6] == 0x1A
+					&& bytes[7] == 0x0A
+				if (!pngData.headerIsValid) {
+					console.warn('Provided data does not belong to a png');
+					return pngData;
+				}
+				//parsing chunks
+				var chunks = [];
+				var chunk = PNG.utils.parseChunk(bytes, 8);
+				chunks.push(chunk);
+				while (chunk.name !== 'IEND') {
+					chunk = PNG.utils.parseChunk(bytes, chunk.end);
+					chunks.push(chunk);
+				}
+				pngData['chunks'] = chunks;
+				return pngData;
+			},
+			parseChunk: function(bytes, start) {
+				var chunkLength = PNG.utils.bytes2Int(bytes.slice(start, start + 4));
+
+				var chunkName = '';
+				chunkName += String.fromCharCode(bytes[start + 4]);
+				chunkName += String.fromCharCode(bytes[start + 5]);
+				chunkName += String.fromCharCode(bytes[start + 6]);
+				chunkName += String.fromCharCode(bytes[start + 7]);
+
+				var chunkData = [];
+				for (var idx = start + 8; idx<chunkLength + start + 8; idx++) {
+					chunkData.push(bytes[idx]);
+				}
+				//TODO validate crc as required!
+				return {
+					start: start,
+					end: Number(start) + Number(chunkLength) + 12, //12 = 4 (length) + 4 (name) + 4 (crc)
+					length: chunkLength,
+					name: chunkName,
+					data: chunkData,
+					crc: [
+						bytes[chunkLength + start + 8],
+						bytes[chunkLength + start + 9],
+						bytes[chunkLength + start + 10],
+						bytes[chunkLength + start + 11]
+					],
+					crcChecked: false
+				};
+			},
+			enrichParsedData: function(pngData) {
+				var idhrChunk = PNG.utils.getChunk(pngData, 'IDHR');
+
+				//see http://www.libpng.org/pub/png/spec/1.2/PNG-Chunks.html
+				pngData.width = PNG.utils.bytes2Int(idhrChunk.data.slice(0, 4));
+				pngData.height = PNG.utils.bytes2Int(idhrChunk.data.slice(4, 8));
+				pngData.bitDepth = PNG.utils.bytes2Int(idhrChunk.data.slice(8, 9));
+				pngData.colorType = PNG.utils.bytes2Int(idhrChunk.data.slice(9, 10));
+				pngData.compressionMethod = PNG.utils.bytes2Int(idhrChunk.data.slice(10, 11));
+				pngData.filterMethod = PNG.utils.bytes2Int(idhrChunk.data.slice(11, 12));
+				pngData.interlaceMethod = PNG.utils.bytes2Int(idhrChunk.data.slice(12, 13));
+
+				pngData.isGreyScale = pngData.colorType == 0 || pngData.colorType == 4;
+				pngData.isRgb = pngData.colorType == 2 || pngData.colorType == 6;
+				pngData.hasAlpha = pngData.colorType == 4 || pngData.colorType == 6;
+				pngData.hasPaletteMode = pngData.colorType == 3 && PNG.utils.getChunk(pngData, 'PLTE') != null;
+				return pngData;
+			},
+			getChunks: function(pngData, chunkName) {
+				var chunksForName = [];
+				for (var idx = 0; idx<pngData.chunks.length; idx++) {
+					if (pngData.chunks[idx].name = chunkName) {
+						chunksForName.push(pngData.chunks[idx]);
+					}
+				}
+				return chunksForName;
+			},
+			getChunk: function(pngData, chunkName) {
+				for (var idx = 0; idx<pngData.chunks.length; idx++) {
+					if (pngData.chunks[idx].name = chunkName) {
+						return pngData.chunks[idx];
+					}
+				}
+				return null;
+			},
+			bytes2Int: function(bytes) {
+				var ret = 0;
+				for (var idx = 0; idx<bytes.length; idx++) {
+					ret += bytes[idx];
+					if (idx < bytes.length - 1) {
+						ret = ret << 8;
+					}
+				}
+				return ret;
+			}
+		}
+	}
+
+	// used as follows
+	let t0 = nowFn()
+	var pngData = PNG.parse(target);
+	let tEnd = nowFn()
+	console.log(tEnd-t0, 'ms', pngData);
+}
+
 const get_canvas = () => new Promise(resolve => {
 
 	const sizeW = 16, sizeH = 8, pixelcount = sizeW * sizeH, allZeros = '93bd94c5'
 	// FF95+: compression 1724331 / 1737038 
+	// libz-rs
+  	// FF137 1910796: Enable libz-rs on nightly: this changes our known hashes
+  	// FF139 1949947: Upgrade zlib-rs/libz-rs-sys to 0.4.2. (new to*_solids)
 	const oKnown = {
 		'ge_white': ['d5f8f171'],
 		'isPointInPath': ['db0e3f08'],
 		'isPointInStroke': ['a77e328a'],
-		'to_white': ['35e41537'],
-		'toBlob': ['3afc375a'],
-		'toBlob_solid': ['56ea6104'],
-		'toDataURL': ['3afc375a'],
-		'toDataURL_solid': ['56ea6104'],
-	}
-	// libz-rs
-  	// FF137 1910796: Enable libz-rs on nightly: this changes our known hashes
-  	// FF139 1949947: Upgrade zlib-rs/libz-rs-sys to 0.4.2. (new to*_solids)
-	if (isVer > 136) {
-		oKnown['toBlob'].push('e328ec8e')
-		oKnown['toBlob_solid'].push('9d0b9932','cfd52a1f')
-		oKnown['toDataURL'].push('e328ec8e')
-		oKnown['toDataURL_solid'].push('9d0b9932','cfd52a1f')
-		oKnown['to_white'].push('3e72d1fd')
+		'to_white': ['35e41537','3e72d1fd'],
+		'toBlob': ['3afc375a','e328ec8e'],
+		'toBlob_solid': ['56ea6104','9d0b9932','cfd52a1f'],
+		'toDataURL': ['3afc375a','e328ec8e'],
+		'toDataURL_solid': ['56ea6104','9d0b9932','cfd52a1f'],
 	}
 	let isCanvasGet ='', isCanvasGetChannels ='', isGetStealth = false
 
