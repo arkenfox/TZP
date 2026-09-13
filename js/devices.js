06c7067e664e71c661f4e19d615d63b552b3727a
Thorin-II <16656956+Thorin-II@users.noreply.github.com>
2026-09-13 18:45:14 +0000

    tweak

--- js/devices.js
+++ js/devices.js
@@ -817,40 +817,6 @@ function get_touc_h(METRIC) {
 	return
 }
 
-function get_viewport_segments(METRIC) {
-	// https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_media_queries
-	// CSS level 5
-
-	let data = {}, display = {}, aList = ['horizontal','vertical']
-	aList.forEach(function(m) {
-		let value = zNA
-		try {
-			for (let i = 1; i < 6; i++) { // css only goes to 5
-				if (window.matchMedia('('+ m +'-viewport-segments:'+ i +')').matches) {value = i; break}
-			}
-			if (runSE) {foo++} else if (runSL) {value = 6}
-		} catch(e) {
-			value = zErr; log_error(7, METRIC +'_'+ m, e)
-		}
-		let pseudo = 'horizontal' == m ? ':before' : ':after'
-		let cssvalue = getElementProp(7, '#cssVS', METRIC +'_'+ m +'_css', pseudo)
-		display[m] = value
-		if (isSmart) {
-			if (cssvalue !== zErr && value !== zErr) {
-				if (value !== cssvalue) {
-					display[m] = log_known(7, METRIC +'_'+ m, value) // record and color up lies
-					value = zLIE
-				}
-			}
-		}
-		data[m] = value
-		data[m +'_css'] = cssvalue
-	})
-	addDisplay(7, METRIC, display.horizontal +' x '+ display.vertical)
-	addData(7, METRIC, data, mini(data))
-	return
-}
-
 const outputDevices = () => new Promise(resolve => {
 	if (gRun && sectionIgnore.includes('devices')) {return resolve()}
 
@@ -863,7 +829,6 @@ const outputDevices = () => new Promise(resolve => {
 		get_device_integer('hardwareConcurrency','Navigator.'),
 		get_permissions('permissions'),
 		get_permissions_policy('permissionsPolicy'), // blink only | gecko behind a pref since FF65
-		get_viewport_segments('viewport-segments'),
 		// blink only
 		get_battery('battery'),
 		get_device_memory('deviceMemory'),
