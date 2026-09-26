b042c354b473c966b3ca23a8a98138f8fa7114e1
Thorin-II <16656956+Thorin-II@users.noreply.github.com>
2026-09-26 07:38:35 +0000

    tweak (#613)

--- js/globals.js
+++ js/globals.js
@@ -22,7 +22,7 @@ const btnList = ['alerts','debug','errors','lies']
 
 const jsFilesExpected = 15,
 	gSectionsExpected = 16,
-	expectedMetrics = 137
+	expectedMetrics = 138
 let jsFiles = 0, gCount = 0, gCountTiming = 0
 
 // global
